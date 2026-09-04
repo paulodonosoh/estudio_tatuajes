f55363d4e13dbf5c491d78bcb5409e4d91e93225
Israel <israel.vargas02@inacapmail.cl>
2026-09-04 02:49:15 -0400

    Agrega imagenes estáticas locales y estructura completa de artistas

--- galeria/views.py
+++ galeria/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 
 def lista_tatuajes(request):
-    tatuajes = [
-        {'nombre': 'Rosa Tradicional', 'estilo': 'Old School'},
-        {'nombre': 'Lobo Geométrico', 'estilo': 'Blackwork'},
-        {'nombre': 'Dragón Japonés', 'estilo': 'Irezumi'}
+    artistas = [
+        {
+            'persona': 'Israel Vargas',
+            'estilo': 'Old School',
+            'creados': ['Rosa Tradicional', 'Ancla clásica'],
+            'puede_hacer': ['Pantera Negra', 'Daga Tradicional'],
+            'imagen': 'galeria/img/tatuaje1.jpg'
+        },
+        {
+            'persona': 'Paulo Donoso',
+            'estilo': 'Blackwork',
+            'creados': ['Lobo Geométrico', 'Mandalas'],
+            'puede_hacer': ['Geometría Sagrada', 'Dotwork'],
+            'imagen': 'galeria/img/tatuaje2.jpg'
+        },
+        {
+            'persona': 'Noah Del Desposito',
+            'estilo': 'Irezumi',
+            'creados': ['Dragón Japonés', 'Koi Fish'],
+            'puede_hacer': ['Hannya Mask', 'Samurai'],
+            'imagen': 'galeria/img/tatuaje3.jpg'
+        }
     ]
     
     context = {
-        'tatuajes': tatuajes,
-        'titulo_seccion': 'Nuestra Galería de Trabajos'
+        'artistas': artistas,
+        'titulo_seccion': 'Nuestros Artistas y Trabajos'
     }
     return render(request, 'galeria/galeria.html', context)
