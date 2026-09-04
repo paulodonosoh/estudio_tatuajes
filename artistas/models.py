4bb373b2c824e4cea5b6c021953dabc4fe91ea57
florencia2026 <florenciadeltam@gmail.com>
2026-09-04 19:45:31 -0400

    agregar imagenes de artistas

--- artistas/models.py
+++ artistas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Artista(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+    imagen = models.ImageField(upload_to='artistas/', blank=True, null=True)
+    url = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.nombre
